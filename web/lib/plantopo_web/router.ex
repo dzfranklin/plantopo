d7e21e3949a5719a68b9dd172ddd4cf69835e4ab
Daniel Franklin <daniel@danielzfranklin.org>
2023-01-29 09:03:06 -0700

    Add auth
    - mix phx.gen.auth
    - remove examples from docs in context for brevity
    - refactor router

--- web/lib/plantopo_web/router.ex
+++ web/lib/plantopo_web/router.ex
@@ -1,6 +1,8 @@
 defmodule PlanTopoWeb.Router do
   use PlanTopoWeb, :router
 
+  import PlanTopoWeb.UserAuth
+
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
@@ -8,23 +10,59 @@ defmodule PlanTopoWeb.Router do
     plug :put_root_layout, {PlanTopoWeb.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug :fetch_current_user
   end
 
   pipeline :api do
     plug :accepts, ["json"]
   end
 
-  scope "/", PlanTopoWeb do
-    pipe_through :browser
-
-    get "/", PageController, :home
-    get "/map", MapController, :index
-  end
-
   # Other scopes may use custom stacks.
   # scope "/api", PlanTopoWeb do
   #   pipe_through :api
   # end
+
+  ## Auth optional
+  scope "/", PlanTopoWeb do
+    pipe_through [:browser]
+
+    get "/", PageController, :home
+    delete "/users/log_out", UserSessionController, :delete
+
+    live_session :current_user,
+      on_mount: [{PlanTopoWeb.UserAuth, :mount_current_user}] do
+      live "/users/confirm/:token", UserConfirmationLive, :edit
+      live "/users/confirm", UserConfirmationInstructionsLive, :new
+    end
+  end
+
+  ## Auth forbidden
+  scope "/", PlanTopoWeb do
+    pipe_through [:browser, :redirect_if_user_is_authenticated]
+
+    live_session :redirect_if_user_is_authenticated,
+      on_mount: [{PlanTopoWeb.UserAuth, :redirect_if_user_is_authenticated}] do
+      live "/users/register", UserRegistrationLive, :new
+      live "/users/log_in", UserLoginLive, :new
+      live "/users/reset_password", UserForgotPasswordLive, :new
+      live "/users/reset_password/:token", UserResetPasswordLive, :edit
+    end
+
+    post "/users/log_in", UserSessionController, :create
+  end
+
+  ## Auth required
+  scope "/", PlanTopoWeb do
+    pipe_through [:browser, :require_authenticated_user]
+
+    get "/map", MapController, :index
+
+    live_session :require_authenticated_user,
+      on_mount: [{PlanTopoWeb.UserAuth, :ensure_authenticated}] do
+      live "/users/settings", UserSettingsLive, :edit
+      live "/users/settings/confirm_email/:token", UserSettingsLive, :confirm_email
+    end
+  end
 
   # Enable LiveDashboard and Swoosh mailbox preview in development
   if Application.compile_env(:plantopo, :dev_routes) do
