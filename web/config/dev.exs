c057d5e3c5dbafc90094a79bf7d97fd70f46f170
Daniel Franklin <daniel@danielzfranklin.org>
2023-02-04 17:44:45 -0700

    Revert "Start server by default in dev env"
    This starts the server whenever we run any mix command.
    
    This reverts commit faaa3e7d088545b9910092cd45226479bfc66b69.

--- web/config/dev.exs
+++ web/config/dev.exs
@@ -20,7 +20,6 @@ config :plantopo, PlanTopoWeb.Endpoint,
   # Binding to loopback ipv4 address prevents access from other machines.
   # Change to `ip: {0, 0, 0, 0}` to allow access from other machines.
   http: [ip: {0, 0, 0, 0}, port: 4000],
-  server: true,
   check_origin: false,
   code_reloader: true,
   debug_errors: true,
