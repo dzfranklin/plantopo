69c3a208a45aa324c91cfe543beb580f524b76f0
Daniel Franklin <daniel@danielzfranklin.org>
2023-02-21 14:16:38 -0700

    Configure eslint

---
+++ web/assets/.eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  overrides: [
+  ],
+  ignorePatterns: ["/vendor", "/node_modules", ".*", "*.config.js"],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['tsconfig.json']
+  },
+  plugins: [
+    'react'
+  ],
+  rules: {
+    quotes: ['warn', 'single'],
+    semi: ['warn', 'always'],
+    'comma-dangle': ['warn', 'always-multiline'],
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    'prefer-const': 'warn',
+    'no-empty': 'warn',
+  }
+};
