e19d138c55fd1af874065371164f82c617f80ec8
Daniel Franklin <daniel@danielzfranklin.org>
2023-03-04 12:56:35 -0700

    generate.sh: run eslint --fix

--- web/assets/js/map/components/icons/generate.sh
+++ web/assets/js/map/components/icons/generate.sh
@@ -4,3 +4,5 @@ npx @svgr/cli --out-dir . -- ../../../../../../design/icons
 rm -f ./*.tsx
 rename 's/\.js$/.tsx/' ./*Icon.js
 mv index.js index.ts
+pushd ../../../../ && npx eslint js/map/components/icons --fix
+popd || exit 1
