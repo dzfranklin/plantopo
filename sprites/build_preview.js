13e1ee28ddfb93e6e8f994f36d7719274aa6120a
Daniel Franklin <daniel@danielzfranklin.org>
2023-03-04 11:18:45 -0700

    Build feature sprites

---
+++ sprites/build_preview.js
@@ -0,0 +1,20 @@
+const { optimize } = require('svgo');
+const fs = require('fs');
+
+const out = {};
+
+fs.readdirSync(__dirname + '/svg').forEach((file) => {
+  const fpath = __dirname + '/svg/' + file;
+  const svg = fs.readFileSync(fpath, 'utf8');
+  const fname = fpath.split('/').pop().split('.')[0];
+
+  const optimized = optimize(svg, {
+    path: fpath,
+    multipass: true,
+  });
+
+  out[fname] = optimized;
+});
+
+
+fs.writeFileSync(__dirname + '/preview.json', JSON.stringify(out, null, 2));
--- web/lib/plantopo_web.ex
+++ web/lib/plantopo_web.ex
@@ -16,7 +16,7 @@ defmodule PlanTopoWeb do
   below. Instead, define additional modules and import
   those modules here.
   """
-  def static_paths, do: ~w(assets fonts images favicon.ico robots.txt)
+  def static_paths, do: ~w(assets sprite fonts images favicon.ico robots.txt)
 
   def router do
     quote do
