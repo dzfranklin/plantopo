884db486ad017a9a9dd720632a54ad38259045e5
Daniel Franklin <daniel@danielzfranklin.org>
2023-03-05 23:59:57 -0700

    Re-enable sql logging

--- web/config/dev.exs
+++ web/config/dev.exs
@@ -32,8 +32,7 @@ config :plantopo, PlanTopo.Repo,
   database: "plantopo_dev",
   stacktrace: true,
   show_sensitive_data_on_connection_error: true,
-  pool_size: 10,
-  log: false
+  pool_size: 10
 
 # For development, we disable any cache and enable
 # debugging and code reloading.
