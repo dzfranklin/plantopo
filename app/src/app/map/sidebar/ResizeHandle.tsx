cc0e92ff2957e5343af446204ef773edfcaf1d19
Daniel Franklin <daniel@danielzfranklin.org>
2023-08-22 00:03:14 -0700

    bugfix

--- app/src/app/map/sidebar/ResizeHandle.tsx
+++ app/src/app/map/sidebar/ResizeHandle.tsx
@@ -8,6 +8,12 @@ import {
 
 const WIDTH_MIN = 60;
 
+const END_EVENTS: Array<'pointerup' | 'pointercancel' | 'pointerleave'> = [
+  'pointerup',
+  'pointercancel',
+  'pointerleave',
+];
+
 export function ResizeHandle({
   setWidth,
 }: {
@@ -31,10 +37,12 @@ export function ResizeHandle({
   }, []);
   const onEnd = useCallback(
     (evt: globalThis.PointerEvent) => {
-      window.removeEventListener('pointermove', onPointerMove);
-      window.removeEventListener('pointerup', onEnd);
-      window.removeEventListener('pointercancel', onEnd);
-      window.removeEventListener('pointerleave', onEnd);
+      window.removeEventListener('pointermove', onPointerMove, {
+        capture: true,
+      });
+      for (const evt of END_EVENTS) {
+        window.removeEventListener(evt as any, onEnd, { capture: true });
+      }
       dragAnimNext.current = null;
       ref.current?.classList.remove('bg-blue-400');
       evt.preventDefault();
@@ -45,9 +53,9 @@ export function ResizeHandle({
   const startDrag = useCallback(
     (evt: PointerEvent<HTMLDivElement>) => {
       addEventListener('pointermove', onPointerMove, { capture: true });
-      addEventListener('pointerup', onEnd, { capture: true });
-      addEventListener('pointercancel', onEnd, { capture: true });
-      addEventListener('pointerleave', onEnd, { capture: true });
+      for (const evt of END_EVENTS) {
+        addEventListener(evt, onEnd, { capture: true });
+      }
       dragAnimNext.current = evt.clientX;
       ref.current?.classList.add('bg-blue-400');
       evt.preventDefault();
