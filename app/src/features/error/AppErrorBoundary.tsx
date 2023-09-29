b9f6e6290e58b98a8ab2753cfbd76666ca43b8c5
Daniel Franklin <daniel@danielzfranklin.org>
2023-09-29 14:32:15 -0700

    Write error boundary alert from scratch

--- app/src/features/error/AppErrorBoundary.tsx
+++ app/src/features/error/AppErrorBoundary.tsx
@@ -1,35 +1,29 @@
 'use client';
 
-import { AlertDialog, DialogContainer } from '@adobe/react-spectrum';
 import { ErrorBoundary as ReactErrorBoundary } from 'react-error-boundary';
 import ErrorTechInfo from './ErrorTechInfo';
 import { ErrorInfo } from 'react';
-import {
-  defaultTheme as defaultSpectrumTheme,
-  Provider as SpectrumProvider,
-} from '@adobe/react-spectrum';
+import { XCircleIcon } from '@heroicons/react/20/solid';
 
-function fallbackRender({ error }: { error: unknown }) {
+export default function fallbackRender({ error }: { error: unknown }) {
   return (
-    <SpectrumProvider
-      theme={defaultSpectrumTheme}
-      // Set render consistently on the server so Next.js can
-      // rehydrate. Is there a better way to do this?
-      locale="en-US"
-      scale="medium"
-      minHeight="100vh"
-    >
-      <DialogContainer isDismissable={false} onDismiss={() => {}}>
-        <AlertDialog
-          title="Error"
-          variant="error"
-          primaryActionLabel="Reload"
-          onPrimaryAction={() => document.location.reload()}
-        >
+    <div className="max-w-2xl p-4 mx-auto mt-12 rounded-md bg-red-50">
+      <div className="flex">
+        <div className="flex-shrink-0">
+          <XCircleIcon className="w-5 h-5 text-red-400" aria-hidden="true" />
+        </div>
+        <div className="ml-3">
+          <h3 className="text-sm font-medium text-red-800">Critical error</h3>
+          <div className="my-2 text-sm text-red-700">
+            <p>
+              An unexpected error has occured. Please contact me at
+              daniel@plantopo.com.
+            </p>
+          </div>
           <ErrorTechInfo error={error} />
-        </AlertDialog>
-      </DialogContainer>
-    </SpectrumProvider>
+        </div>
+      </div>
+    </div>
   );
 }
 
--- app/src/features/error/ErrorTechInfo.tsx
+++ app/src/features/error/ErrorTechInfo.tsx
@@ -1,8 +1,10 @@
-import { ActionButton } from '@adobe/react-spectrum';
 import { useMemo } from 'react';
 
 /** Displays technical information suitable for showing to the user so that it
- * can be reported to us */
+ * can be reported to us.
+ *
+ * NOTE: Must not depend on being run in any provider so that we can use in an
+ * any error boundary. In particular this means it can't use spectrum components. */
 export default function ErrorTechInfo({ error }: { error: unknown }) {
   const json = useMemo(() => {
     return JSON.stringify(
@@ -31,9 +33,13 @@ export default function ErrorTechInfo({ error }: { error: unknown }) {
         <code>{json}</code>
       </pre>
 
-      <ActionButton onPress={() => console.info({ error })}>
+      <button
+        type="button"
+        className="px-2 py-1 text-xs font-semibold text-gray-900 bg-white rounded shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+        onClick={() => console.info({ error })}
+      >
         Log to console
-      </ActionButton>
+      </button>
     </details>
   );
 }
