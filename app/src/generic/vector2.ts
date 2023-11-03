c99eaef646cc057f0e62e986f0ec91a6a5790a89
Daniel Franklin <daniel@danielzfranklin.org>
2023-11-03 10:29:19 +0000

    Render fixes (#98)
    * Handle turfjs returning infinity
    
    * don't require subpixel aliasing in lines

--- app/src/generic/vector2.ts
+++ app/src/generic/vector2.ts
@@ -15,3 +15,7 @@ export function sub2(
 export function magnitude2(a: [number, number]): number {
   return Math.sqrt(a[0] * a[0] + a[1] * a[1]);
 }
+
+export function floor2(a: [number, number]): [number, number] {
+  return [Math.floor(a[0]), Math.floor(a[1])];
+}
