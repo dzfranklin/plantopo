8715ba78cb033b00bbd12cd914d5ed212704834c
Daniel Franklin <daniel@danielzfranklin.org>
2023-11-06 20:52:24 +0000

    Fix lint (#141)

--- app/src/features/map/manager/MapsManagerSection.tsx
+++ app/src/features/map/manager/MapsManagerSection.tsx
@@ -134,7 +134,7 @@ function ShareButton({ item }: { item: MapMeta }) {
       <ActionButton aria-label="share" isQuiet>
         <ShareIcon />
       </ActionButton>
-      {(close) => <MapShareDialog item={item} />}
+      <MapShareDialog item={item} />
     </DialogTrigger>
   );
 }
