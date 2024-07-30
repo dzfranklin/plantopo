e41de5b250b9388c428188b63fd3ba6d41e0a398
Daniel Franklin <daniel@danielzfranklin.org>
2024-07-30 20:28:11 +0100

    Add check workflow

--- backend/internal/ordnancesurvey/proj_test.go
+++ backend/internal/ordnancesurvey/proj_test.go
@@ -1,15 +1,45 @@
 package ordnancesurvey
 
 import (
+	"bytes"
+	"fmt"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"github.com/twpayne/go-proj/v10"
+	"os"
+	"os/exec"
+	"strings"
 	"testing"
 )
 
 func TestNewFromBNG(t *testing.T) {
+	srcX := 216666.0
+	srcY := 771288.0
+
 	pj := newFromBNG()
-	lnglat, err := pj.Forward(proj.NewCoord(216666, 771288, 0, 0))
+	lnglat, err := pj.Forward(proj.NewCoord(srcX, srcY, 0, 0))
 	require.NoError(t, err)
-	require.InDelta(t, lnglat.Y(), -5.003683, 0.0000009) // longitude
-	require.InDelta(t, lnglat.X(), 56.796887, 0.0000009) // latitude
+	// We use a large delta so this test passes on proj 8 (what we get if we apt install libproj-dev on GitHub actions)
+	assert.InDelta(t, lnglat.Y(), -5.0037, 0.00009) // longitude
+	assert.InDelta(t, lnglat.X(), 56.7969, 0.00009) // latitude
+
+	if t.Failed() {
+		cmd := exec.Command("proj")
+		var outBuf bytes.Buffer
+		cmd.Stderr = &outBuf
+		if err := cmd.Run(); err != nil {
+			t.Error(err)
+		}
+		fmt.Printf("proj: %s\n", strings.Split(outBuf.String(), "\n")[0])
+
+		cmd = exec.Command("cs2cs", "+init=epsg:27700", "+to", "+init=epsg:4326", "-f", "%.6f")
+		cmd.Stdin = strings.NewReader(fmt.Sprintf("%f %f", srcX, srcY))
+		outBuf = bytes.Buffer{}
+		cmd.Stdout = &outBuf
+		cmd.Stderr = os.Stderr
+		if err := cmd.Run(); err != nil {
+			t.Error(err)
+		}
+		fmt.Printf("%s: %s\n", strings.Join(cmd.Args, " "), outBuf.String())
+	}
 }
