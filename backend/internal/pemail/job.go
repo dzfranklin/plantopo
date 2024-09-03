fc54a2d63c04610cf67972820d97d96e60952160
Daniel Franklin <daniel@danielzfranklin.org>
2024-09-03 19:10:35 +0100

    Skip sending mail if !IsProduction

--- backend/internal/pemail/job.go
+++ backend/internal/pemail/job.go
@@ -5,6 +5,7 @@ import (
 	"github.com/dzfranklin/plantopo/backend/internal/pconfig"
 	"github.com/riverqueue/river"
 	"github.com/wneessen/go-mail"
+	"log/slog"
 )
 
 type JobArgs struct {
@@ -16,29 +17,42 @@ func (j JobArgs) Kind() string {
 }
 
 type Worker struct {
-	c *mail.Client
+	isProduction bool
+	c            *mail.Client
+	l            *slog.Logger
 	river.WorkerDefaults[JobArgs]
 }
 
 func NewWorker(env *pconfig.Env) *Worker {
-	cfg := &env.Config.SMTPRelay
-	c, err := mail.NewClient(cfg.Server, mail.WithPort(cfg.Port),
-		mail.WithSMTPAuth(mail.SMTPAuthLogin), mail.WithUsername(cfg.Username), mail.WithPassword(cfg.Password))
-	if err != nil {
-		panic(err)
+	if env.IsProduction {
+		cfg := &env.Config.SMTPRelay
+		c, err := mail.NewClient(cfg.Server, mail.WithPort(cfg.Port),
+			mail.WithSMTPAuth(mail.SMTPAuthLogin), mail.WithUsername(cfg.Username), mail.WithPassword(cfg.Password))
+		if err != nil {
+			panic(err)
+		}
+		return &Worker{isProduction: true, c: c, l: env.Logger}
+	} else {
+		return &Worker{isProduction: false, l: env.Logger}
 	}
-	return &Worker{c: c}
 }
 
 func (w *Worker) Work(ctx context.Context, job *river.Job[JobArgs]) error {
+	msg := job.Args.Msg
+
+	if !w.isProduction {
+		w.l.Info("would mail in prod", "to", msg.To, "subject", msg.Subject, "text", msg.Text)
+		return nil
+	}
+
 	m := mail.NewMsg()
 	if err := m.FromFormat(fromName, fromAddress); err != nil {
 		return err
 	}
-	if err := m.To(job.Args.Msg.To); err != nil {
+	if err := m.To(msg.To); err != nil {
 		return err
 	}
-	m.Subject(job.Args.Msg.Subject)
-	m.SetBodyString(mail.TypeTextPlain, job.Args.Msg.Text)
+	m.Subject(msg.Subject)
+	m.SetBodyString(mail.TypeTextPlain, msg.Text)
 	return w.c.DialAndSendWithContext(ctx, m)
 }
