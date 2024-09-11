f9ec4b0342eeffe9f5bcdb46d83d5c25e0fe5e21
Daniel Franklin <daniel@danielzfranklin.org>
2024-09-11 16:12:56 +0100

    add co

---
+++ backend/migrations/016_geophotos_colorado.sql
@@ -0,0 +1,8 @@
+INSERT INTO flickr_index_regions (name, min_lng, min_lat, max_lng, max_lat)
+VALUES ('colorado', -109.024750, 37.008039, -102.071179, 40.991509)
+
+---- create above / drop below ----
+
+DELETE
+FROM flickr_index_regions
+WHERE name = 'colorado';
