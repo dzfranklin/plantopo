8bed40c5a67b61c8dbca7d6501a0836a16a0bbfd
Daniel Franklin <daniel@danielzfranklin.org>
2024-10-01 19:46:47 +0100

    visual bugfix: center ruler icon

--- app/features/map/LinearMeasureControl.ts
+++ app/features/map/LinearMeasureControl.ts
@@ -13,7 +13,7 @@ const sourceID = prefix + 'source';
 const activeClass = 'text-blue-600';
 
 // credit: https://github.com/korywka/mapbox-controls (MIT license)
-const icon =
+const iconSource =
   '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="23" height="23" fill="currentColor">\n' +
   '<rect fill="none" height="24" width="24"></rect>\n' +
   '<path d="M20,6H4C2.9,6,2,6.9,2,8v8c0,1.1,0.9,2,2,2h16c1.1,0,2-0.9,2-2V8C22,6.9,21.1,6,20,6z M20,16H4V8h3v3c0,0.55,0.45,1,1,1h0 c0.55,0,1-0.45,1-1V8h2v3c0,0.55,0.45,1,1,1h0c0.55,0,1-0.45,1-1V8h2v3c0,0.55,0.45,1,1,1h0c0.55,0,1-0.45,1-1V8h3V16z"></path>\n' +
@@ -61,9 +61,11 @@ export class LinearMeasureControl implements ml.IControl {
   private _b = createElement({
     tag: 'button',
     title: 'Ruler',
-    className: 'mx-auto',
-    innerHTML: icon,
     onclick: () => this._toggle(),
+    contents: {
+      className: 'flex justify-center align-center',
+      innerHTML: iconSource,
+    },
   });
   private _c = createElement({
     className: 'maplibregl-ctrl maplibregl-ctrl-group',
