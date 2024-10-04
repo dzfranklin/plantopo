ced379c629c01bf80212c9dcb53376d2487468af
Daniel Franklin <daniel@danielzfranklin.org>
2024-10-04 13:01:49 +0100

    Fix vitest

--- app/vitest.config.ts
+++ app/vitest.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
+import * as path from 'node:path';
 
 export default defineConfig({
   plugins: [react()],
   test: {
     globalSetup: './vitest.global-setup.ts',
     environment: 'jsdom',
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './'),
+    },
   },
 });
