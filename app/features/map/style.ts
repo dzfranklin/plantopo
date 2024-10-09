1b30871eb43b7e0d07b897f3ba44521a7b08fb8f
Daniel Franklin <daniel@danielzfranklin.org>
2024-10-09 22:59:25 +0100

    Add paper maps overlay

--- app/features/map/style.ts
+++ app/features/map/style.ts
@@ -432,6 +432,77 @@ const overlayStyleList: OverlayStyle[] = [
       },
     ],
   },
+  {
+    id: 'paper-maps',
+    name: 'Paper Maps',
+    sources: {
+      default: {
+        type: 'vector',
+        url: 'pmtiles://https://plantopo-storage.b-cdn.net/paper-maps/paper_maps.pmtiles',
+      },
+    },
+    layers: [
+      {
+        id: 'outline',
+        type: 'line',
+        source: 'default',
+        'source-layer': 'default',
+        minzoom: 4,
+        paint: {
+          'line-color': ['get', 'color'],
+          'line-width': 1.5,
+          'line-opacity': 0.8,
+        },
+      },
+      {
+        id: 'fill',
+        type: 'fill',
+        source: 'default',
+        'source-layer': 'default',
+        minzoom: 5,
+        layout: {},
+        paint: {
+          'fill-color': ['get', 'color'],
+          'fill-opacity': [
+            'case',
+            ['boolean', ['feature-state', 'hover'], false],
+            0.3,
+            0.1,
+          ],
+        },
+      },
+      {
+        id: 'label',
+        type: 'symbol',
+        source: 'default',
+        'source-layer': 'default',
+        layout: {
+          'icon-offset': [0, -0.2],
+          'icon-image': ['get', 'icon'],
+          'icon-allow-overlap': ['step', ['zoom'], false, 6, true],
+          'icon-anchor': 'bottom',
+          'icon-size': ['interpolate', ['linear'], ['zoom'], 5, 0.2, 9, 0.5],
+
+          'text-offset': [0, 0.2],
+          // prettier-ignore
+          'text-field': [
+            'step', ['zoom'],
+            '',
+            7, ['get', 'short_title'],
+            9, ['get', 'title'],
+          ],
+          'text-allow-overlap': true,
+          'text-size': 14,
+          'text-anchor': 'top',
+        },
+        paint: {
+          'text-color': ['get', 'color'],
+          'text-halo-width': 1.4,
+          'text-halo-color': '#fafafa',
+        },
+      },
+    ],
+  },
 ];
 
 export const overlayStyles: Record<string, OverlayStyle> =
