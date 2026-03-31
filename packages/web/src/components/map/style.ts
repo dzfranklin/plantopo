c4313652942f2b8210f6fc77c5d3a10ba78198e4
Daniel Franklin <daniel@danielzfranklin.org>
2026-03-31 18:10:58 +0100

    Put tile endpoints in order
    ml uses them all, and handles order itself

--- packages/web/src/components/map/style.ts
+++ packages/web/src/components/map/style.ts
@@ -118,8 +118,8 @@ export const OS_SOURCE: RasterSourceSpecification = {
 export const SATELLITE_SOURCE: RasterSourceSpecification = {
   type: "raster",
   tiles: [
-    "https://mt2.google.com/vt/lyrs=s&x={x}&y={y}&z={z}",
     "https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}",
+    "https://mt2.google.com/vt/lyrs=s&x={x}&y={y}&z={z}",
     "https://mt3.google.com/vt/lyrs=s&x={x}&y={y}&z={z}",
   ],
   tileSize: 256,
