d82ac973a6d72a8778a3c0f767baf66add14244f
Daniel Franklin <daniel@danielzfranklin.org>
2026-04-12 23:13:12 +0100

    Improve pre-commit hook
    This won't typecheck on only deletions, so it's not perfect, but it will
    make the normal case faster

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,10 @@
 export default {
+  // ts tasks that modify (run sequentially as modifying)
   "**/*.{ts,tsx}": ["eslint --fix", "prettier --write"],
+  // typecheck (run sequentially to use caching)
+  "**/*.ts?(x)": () =>
+    ["shared", "web", "api"].map(
+      pkg => `tsc --noEmit -p packages/${pkg}/tsconfig.json`,
+    ),
   "**/*.{js,mjs,json,css,md}": "prettier --write",
 };
--- lint-staged.tsc.config.js
+++
@@ -1,7 +0,0 @@
-export default {
-  "**/*.ts?(x)": () => [
-    "tsc --noEmit -p packages/shared/tsconfig.json",
-    "tsc --noEmit -p packages/web/tsconfig.json",
-    "tsc --noEmit -p packages/api/tsconfig.json",
-  ],
-};
