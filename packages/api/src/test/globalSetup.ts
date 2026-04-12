1059be4da919a07454aed509d34b88a5eaee2e6d
Daniel Franklin <daniel@danielzfranklin.org>
2026-04-12 19:11:50 +0100

    Fix test teardown

--- packages/api/src/test/globalSetup.ts
+++ packages/api/src/test/globalSetup.ts
@@ -1,7 +1,12 @@
 import "../loadEnv.js";
 
+import { db } from "../db.js";
 import { setupDb } from "./setupDb.js";
 
 export async function setup() {
   await setupDb();
+}
+
+export async function teardown() {
+  await db.$client.end();
 }
