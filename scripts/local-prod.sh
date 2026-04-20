293ec07e959a8ef007237bb52fb4491a99784471
Daniel Franklin <daniel@danielzfranklin.org>
2026-04-20 14:39:57 +0100

    Stub commit hash in local prod

--- scripts/local-prod.sh
+++ scripts/local-prod.sh
@@ -7,5 +7,5 @@ NODE_ENV=production npx tsx scripts/with-api-env.ts > "$api_env_file"
 
 createdb plantopo_prod 2>/dev/null || true
 
-docker build -t plantopo .
+docker build -t plantopo --build-arg "COMMIT_HASH=0000000" .
 docker run --rm -p 3030:4000 --env-file "$api_env_file" plantopo
